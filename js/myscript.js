fdb8db7b87b68de039f5d8c3dbe5fc86d0a2b7cc
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-06 15:49:20 +0100

    feat: implementazione funzione

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+const userKm = parseInt(prompt("Inserisci i km che desideri percorrere"));
+const userAge = parseInt(prompt("Inserisci la tua età"));
+let ticketPrice = userKm * 0.21;
+
+if (userAge < 18) {
+    ticketPrice = ticketPrice - (ticketPrice * 0.2);
+    console.log("Il prezzo del tuo biglietto è: " + ticketPrice.toFixed(2));
+} else if (userAge >= 65) {
+    ticketPrice = ticketPrice - (ticketPrice * 0.4);
+    console.log("Il prezzo del tuo biglietto è: " + ticketPrice.toFixed(2));
+} else {
+    console.log("Il prezzo del tuo biglietto è: " + ticketPrice.toFixed(2));
+}
+
