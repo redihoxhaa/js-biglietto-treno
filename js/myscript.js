46894b17a41da4b859596e97bc4dd8b9c18bc171
redihoxhaa <redi.hoxha1996@gmail.com>
2023-11-07 10:02:09 +0100

    feat: correzione esercizio

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,15 @@
+const pricePerKm = 0.21;
+const discount18 = 0.2;
+const discount65 = 0.4;
 const userKm = parseInt(prompt("Inserisci i km che desideri percorrere"));
 const userAge = parseInt(prompt("Inserisci la tua età"));
-let ticketPrice = userKm * 0.21;
+let ticketPrice = userKm * pricePerKm;
 
 if (userAge < 18) {
-    ticketPrice = ticketPrice - (ticketPrice * 0.2);
+    ticketPrice = ticketPrice - (ticketPrice * discount18);
     console.log("Il prezzo del tuo biglietto è: " + ticketPrice.toFixed(2));
 } else if (userAge >= 65) {
-    ticketPrice = ticketPrice - (ticketPrice * 0.4);
+    ticketPrice = ticketPrice - (ticketPrice * discount65);
     console.log("Il prezzo del tuo biglietto è: " + ticketPrice.toFixed(2));
 } else {
     console.log("Il prezzo del tuo biglietto è: " + ticketPrice.toFixed(2));
